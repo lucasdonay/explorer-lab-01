cad031edb7f429db23a62062d45c775824f42348
Lucas Donay <lucas.donay@gmail.com>
2022-10-18 13:25:24 -0300

    start project

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import "./css/index.css"
+
+const ccColor1 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccColor2 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+
+ccColor1.setAttribute("fill", "red")
+ccColor2.setAttribute("fill", "white")
